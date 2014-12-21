bdbf30510c8ed0e7cb5dd7a7dc65f232a16b827c
joshWyatt <joshuawyatt123@gmail.com>
2014-12-21 09:39:30 -0800

    Make one and poem c files

---
+++ cprogramming.com/lesson1/poem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  printf( "If Jesus is the reason for the season " );
+  getchar();
+  printf( "Then peas mixed with raisins in pleasin' ");
+  getchar();
+  printf( "Then piss can replace snot for sneazin " );
+  getchar();
+  printf( "and this piece on Christmas ain't treason\n" );
+}
