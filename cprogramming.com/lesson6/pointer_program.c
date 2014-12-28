d10793907a06372de36249e79986e0c05a6191e9
joshWyatt <joshuawyatt123@gmail.com>
2014-12-27 16:46:19 -0800

    Rename files

---
+++ cprogramming.com/lesson6/pointer_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int selection;
+  
+
+  printf( "1) Enter a new variable.\n" );
+  printf( "2) See your current variable.\n" );
+  printf( "3) See your variables address in memory.\n" );
+  printf( "4) Here something nice.\n" );
+  scanf("%d\n", &selection);
+}
