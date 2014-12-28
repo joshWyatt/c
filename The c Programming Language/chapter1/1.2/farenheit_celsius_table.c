fab0397135577397f659890911c6ded5497c45c6
joshWyatt <joshuawyatt123@gmail.com>
2014-12-27 17:04:46 -0800

    Add table header

--- The c Programming Language/chapter1/1.2/farenheit_celsius_table.c
+++ The c Programming Language/chapter1/1.2/farenheit_celsius_table.c
@@ -14,6 +14,10 @@ main ()
   step = 20;      /* step size */
 
   fahr = lower;
+
+  /* Print a header above the table */
+  printf("Farenheit/Celsius Table\n\n");
+
   while (fahr <= upper) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
